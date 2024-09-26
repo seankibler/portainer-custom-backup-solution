83e7f09c02cf4cd48d13e91b6e8557602e1ded1e
Sean Kibler <63254+seankibler@users.noreply.github.com>
2024-09-26 02:40:08 -0400

    Dynamically determine database credentials and network

--- backup-wordpress.sh
+++ backup-wordpress.sh
@@ -5,8 +5,8 @@ set -eu
 DATE=$(date +%F)
 STACK=$1
 BACKUP_PATH=/data/backups/$DATE/$STACK
-
-# @todo Dynamically get database name, and docker network or receive as inputs
+WP_CONTAINER_NAME=${STACK}-blog-1
+DB_CONTAINER_NAME=${STACK}-mysql-1
 
 source ./portainer-api.sh
 
@@ -18,24 +18,34 @@ fi
 
 mkdir -p $BACKUP_PATH
 
+#
 # Files
-docker run --rm --volumes-from $STACK-wordpress-1 \
+#
+docker run --rm --volumes-from $WP_CONTAINER_NAME \
 	-v $BACKUP_PATH:/backup debian:latest \
 	tar -czvf /backup/wordpress.tar.gz /var/www/html
 
 
-ROOT_PASSWORD=$(stack_details $STACK | \
-	jq -r '.Env | .[] | select(.name | contains("MYSQL_DATABASE_PASSWORD")) | .value')
+# @refactor this is fairly fragile
+ROOT_PASSWORD=$(docker inspect $DB_CONTAINER_NAME | jq -r '.[0] | .Config.Env | map(select(. | startswith("MYSQL_ROOT_PASSWORD"))) | first | match("MYSQL_ROOT_PASSWORD=(.*+)"; "g") | .captures | first | .string')
+DATABASE_NAME=$(docker inspect $DB_CONTAINER_NAME | jq -r '.[0] | .Config.Env | map(select(. | startswith("MYSQL_DATABASE"))) | first | match("MYSQL_DATABASE=(.*+)"; "g") | .captures | first | .string')
+MYSQL_VERSION=$(docker inspect $DB_CONTAINER_NAME | jq -r '.[0] | .Config.Env | map(select(. | startswith("MYSQL_MAJOR"))) | first | match("MYSQL_MAJOR=(.*+)"; "g") | .captures | first | .string')
+
+# Only works for Portainer WordPress Stack Template available in Portainer API endpoint
+#ROOT_PASSWORD=$(stack_details $STACK | \
+	#jq -r '.Env | .[] | select(.name | contains("MYSQL_DATABASE_PASSWORD")) | .value')
 
 touch /tmp/mysql-$STACK-credential
 chmod 600 /tmp/mysql-$STACK-credential
 echo -e "[client]\npassword=$ROOT_PASSWORD" > /tmp/mysql-$STACK-credential
 
-#Database
+#
+# Database
+#
 docker run --rm -v $BACKUP_PATH:/backup -v /tmp/mysql-$STACK-credential:/root/.my.cnf \
-	--network ${STACK}_default \
-	mysql:5.7 \
-	mysqldump --databases wordpress --host $STACK-db-1 \
+	--network container:$DB_CONTAINER_NAME \
+	mysql:$MYSQL_VERSION \
+	mysqldump --databases $DATABASE_NAME --host $DB_CONTAINER_NAME \
 	--user root \
 	--result-file /backup/wordpress.sql
 
