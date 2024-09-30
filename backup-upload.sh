9a8d9014e60533a54cc0792286a2c6a35c7d00d2
Sean Kibler <63254+seankibler@users.noreply.github.com>
2024-09-30 14:42:58 -0400

    Add script to upload backups to s3-compatible cloud storage

---
+++ backup-upload.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+set -eu
+
+BACKUP_BUCKET_PATH=$1
+BACKUP_PATH=/data/backups
+S3CMD_IMAGE_VERSION=stable
+
+# Use docker image with backups mounted in to upload to S3-compatible storage
+docker run --rm -v $BACKUP_PATH:/backup \
+	-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+	-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+	d3fk/s3cmd:$S3CMD_IMAGE_VERSION \
+	--host $S3_HOST \
+	--host-bucket "$S3_HOST_BUCKET" \
+	sync /backup/ $BACKUP_BUCKET_PATH
+
+# Remove backups older than yesterday from local filesystem
+touch -t $(date --date 'yesterday' +%Y%m%d0000) /tmp/backup-cleanup
+
+find /data/backups/ ! -newer /tmp/backup-cleanup -print0 | xargs -0 rm -rf
+
+rm -f /tmp/backup-cleanup-ref
