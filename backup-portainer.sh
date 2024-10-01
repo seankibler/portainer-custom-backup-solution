7d6fef6d4a4eac55faa06b59a24151ca410b89e3
Sean Kibler <63254+seankibler@users.noreply.github.com>
2024-10-01 16:52:14 -0400

    increase flexibility of portainer backup

--- backup-portainer.sh
+++ backup-portainer.sh
@@ -1,11 +1,22 @@
 #! /usr/bin/env bash
 
+set -e
+
 DATE=$(date +%F)
 BACKUP_PATH=/data/backups/$DATE/portainer
-PAYLOAD=$(jq -n --arg pass "$PORTAINER_BACKUP_PASSWORD" '{password: $pass}')
 
 mkdir -p $BACKUP_PATH
 
-curl -H "X-API-Key:$PORTAINER_API_TOKEN" -d "$PAYLOAD" \
-	-o $BACKUP_PATH/portainer.tar.gz.encrypted \
-	https://$PORTAINER_HOST/api/backup
+if [ -z "${PORTAINER_BACKUP_PASSWORD}" ]; then
+	PAYLOAD=$(jq -n '{password: ""}')
+	curl -H "X-API-Key:$PORTAINER_API_TOKEN" -H 'Content-Type: application/json' \
+		-d "$PAYLOAD" \
+		-o $BACKUP_PATH/portainer.tar.gz \
+		"'${PORTAINER_URL}/api/backup'"
+else
+	PAYLOAD=$(jq -n --arg pass "$PORTAINER_BACKUP_PASSWORD" '{password: $pass}')
+	curl -H "X-API-Key:$PORTAINER_API_TOKEN" -H 'Content-Type: application/json' \
+		-d "$PAYLOAD" \
+		-o $BACKUP_PATH/portainer.tar.gz.encrypted \
+		"'${PORTAINER_URL}/api/backup'"
+fi
