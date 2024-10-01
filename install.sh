09f7d809ba3ef9ccda7017853eeac64d9da1730e
Sean Kibler <63254+seankibler@users.noreply.github.com>
2024-10-01 10:09:42 -0400

    Improve install script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/env bash
 
 # Copy files into place
-cp ./backup.sh ./backup-wordpress.sh ./backup-nextcloud.sh ./backup-upload.sh \
+cp -v ./backup.sh ./backup-wordpress.sh ./backup-nextcloud.sh ./backup-upload.sh \
 	./backup-portainer.sh ./backup-traefik.sh /usr/local/bin/
+#Set ownership
+
+chown root:root /usr/local/bin/backup.sh /usr/local/bin/backup-wordpress.sh \
+	/usr/local/bin/backup-nextcloud.sh /usr/local/bin/backup-upload.sh \
+	/usr/local/bin/backup-portainer.sh /usr/local/bin/backup-traefik.sh
 
 # Set execute permission
 chmod 755 /usr/local/bin/backup.sh /usr/local/bin/backup-wordpress.sh \
