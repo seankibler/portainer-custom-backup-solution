816525eb6580cbee4d6a5b8b989d8b0b261f3dd5
Sean Kibler <63254+seankibler@users.noreply.github.com>
2024-10-02 17:06:12 -0400

    fix recover script name in install script

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ install -v -m 0755 -o root -g root -t /usr/local/bin/ \
 	backup-nextcloud.sh backup-portainer.sh \
 	backup-traefik.sh  backup-upload.sh  \
 	backup-wordpress.sh  backup.sh retrieve.sh \
-	recover-wordpress.sh
+	restore-wordpress.sh
