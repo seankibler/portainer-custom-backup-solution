05b96448794af3ecf03687bf814a6705ed8d5f40
Sean Kibler <63254+seankibler@users.noreply.github.com>
2024-10-02 16:58:55 -0400

    Refactor install script

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
 #! /usr/bin/env bash
 
-# Copy files into place
-cp -v ./backup.sh ./backup-wordpress.sh ./backup-nextcloud.sh ./backup-upload.sh \
-	./backup-portainer.sh ./backup-traefik.sh /usr/local/bin/
-#Set ownership
-
-chown root:root /usr/local/bin/backup.sh /usr/local/bin/backup-wordpress.sh \
-	/usr/local/bin/backup-nextcloud.sh /usr/local/bin/backup-upload.sh \
-	/usr/local/bin/backup-portainer.sh /usr/local/bin/backup-traefik.sh
-
-# Set execute permission
-chmod 755 /usr/local/bin/backup.sh /usr/local/bin/backup-wordpress.sh \
-	/usr/local/bin/backup-nextcloud.sh /usr/local/bin/backup-upload.sh \
-	/usr/local/bin/backup-portainer.sh /usr/local/bin/backup-traefik.sh
+install -v -m 0755 -o root -g root -t /usr/local/bin/ \
+	backup-nextcloud.sh backup-portainer.sh \
+	backup-traefik.sh  backup-upload.sh  \
+	backup-wordpress.sh  backup.sh retrieve.sh \
+	recover-wordpress.sh
