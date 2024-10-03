758bb3dded4be40087874b2024769e596cf45c8a
Sean Kibler <63254+seankibler@users.noreply.github.com>
2024-10-03 14:31:12 -0400

    Help check interferes with undefined var check and these scripts do not need usage

--- backup-portainer.sh
+++ backup-portainer.sh
@@ -2,15 +2,6 @@
 
 set -eu
 
-function usage() {
-	echo "Usage: $0" 2>&1
-}
-
-if [ "$1" = "-h" -o "$1" = "--help" ]; then
-	usage
-	exit 0
-fi
-
 DATE=$(date +%F)
 BACKUP_PATH=/data/backups/$DATE/portainer
 
